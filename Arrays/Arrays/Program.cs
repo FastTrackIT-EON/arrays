f44ac89af89b925ed3dce39293d1fe7850db2b78
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-04-23 12:04:07 +0300

    example with unidimensional arrays

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -10,11 +10,32 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            int nrOfElements = ReadNumber("Array size", 0);
 
-            int[] arrayOfInt = { 1, 2, 3, 4, 5 };
+            int[] arrayOfInt = new int[nrOfElements];
 
+            for (int i = 0; i  < nrOfElements; i++)
+            {
+                int element = ReadNumber("Element at index " + i, 0);
 
-            string[] arrayOfString = new string[10];
+                arrayOfInt[i] = element;
+            }
+
+            Console.WriteLine("First element=" + arrayOfInt[0]);
+            Console.WriteLine("Last element=" + arrayOfInt[arrayOfInt.Length - 1]);
+           
+            int medianIndex = (0 + arrayOfInt.Length - 1) / 2;
+            if (medianIndex >= 0 && medianIndex < arrayOfInt.Length)
+            {
+                Console.WriteLine("Median element=" + arrayOfInt[medianIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Cannot calculate median element");
+            }
+
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
         }
 
         static int ReadNumber(string label, int defaultValue)
