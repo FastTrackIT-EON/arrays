e28810f3ae5d353baa62a493f02b0177330762b1
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-04-23 11:48:38 +0300

    initial project setup + read number function

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrayOfInt = { 1, 2, 3, 4, 5 };
+
+
+            string[] arrayOfString = new string[10];
+        }
+
+        static int ReadNumber(string label, int defaultValue)
+        {
+            Console.Write(label + "=");
+            string value = Console.ReadLine();
+            
+            int result;
+            if (int.TryParse(value, out result))
+            {
+                // parsing succeeded => return parsed result
+                return result;
+            }
+
+            // parsing failed => return defaultValue
+            return defaultValue;
+        }
+    }
+}
